6a1a251c9b598a987911193e5aa22a2e93121d4e
akshayagrawal87 <akshayagrawal87@gmail.com>
2020-10-17 14:15:59 +0530

    made changes and got countries now

--- day11/main.js
+++ day11/main.js
@@ -1,326 +1,124 @@
 //Api key for weather Api
 const API_KEY = "a3aef5040eb809f3fdd82cef7a238b8e";
 
-//IIFE TO GENERATE BASIC STUCTURE OF HTML
-(() => {
-	//Adding main section
-	let main = document.createElement("main");
-	document.body.appendChild(main);
+//Adding main section
+let main = document.createElement("main");
+document.body.appendChild(main);
 
-	//Adding container div
-	let container = document.createElement("div");
-	container.classList.add("container-fluid");
-	main.appendChild(container);
+//Adding container div
+let container = document.createElement("div");
+container.classList.add("container");
+main.appendChild(container);
 
-	//Adding Row Div
-	let row = document.createElement("div");
-	row.classList.add("row");
-	container.appendChild(row);
+//Adding Row Div
+let row = document.createElement("div");
+row.classList.add("row");
+container.appendChild(row);
+let h1 = document.createElement("h1");
+h1.classList.add("text-center", "col-12");
+h1.innerText = "Weather App";
+row.appendChild(h1);
 
-	//Adding col Divs
-	let col1 = document.createElement("div");
-	col1.classList.add("col-lg", "col-sm-12", "afg");
+//Function to GENERATE  STUCTURE OF HTMl Design
+function AddCountry(country) {
+	try {
+		//Adding col Divs
+		let col = document.createElement("div");
+		col.classList.add("col-lg-4", "col-sm-12", "country");
 
-	let col2 = document.createElement("div");
-	col2.classList.add("col-lg", "col-sm-12", "nepal");
+		row.appendChild(col);
 
-	let col3 = document.createElement("div");
-	col3.classList.add("col-lg", "col-sm-12", "srilanka");
+		let card = document.createElement("div");
+		card.classList.add("card", "text-center");
 
-	row.appendChild(col1);
-	row.appendChild(col2);
-	row.appendChild(col3);
-})();
+		let cardBody = document.createElement("div");
+		cardBody.classList.add("card-body");
 
-//IIFE TO GENERATE CARD FOR AFG
-(() => {
-	let afg = document.querySelector(".afg");
+		let cardHeader = document.createElement("div");
+		cardHeader.classList.add("card-header");
 
-	let card = document.createElement("div");
-	card.classList.add("card", "text-center");
+		card.appendChild(cardHeader);
 
-	let cardBody = document.createElement("div");
-	cardBody.classList.add("card-body");
+		let img = document.createElement("img");
+		img.classList.add("card-img-top", "img-fluid", "p-3");
+		img.setAttribute("src", country.flag);
+		card.appendChild(img);
 
-	let cardHeader = document.createElement("div");
-	cardHeader.classList.add("card-header");
+		card.appendChild(cardBody);
+		col.appendChild(card);
 
-	card.appendChild(cardHeader);
+		cardHeader.innerText = country.name;
 
-	let img = document.createElement("img");
-	img.classList.add("card-img-top");
-	img.setAttribute("src", "https://www.countryflags.io/af/shiny/64.png");
-	card.appendChild(img);
+		let cardText = document.createElement("div");
+		cardText.classList.add("card-text");
+		cardBody.appendChild(cardText);
 
-	card.appendChild(cardBody);
-	afg.appendChild(card);
+		let capt = document.createElement("div");
+		let capital = "Capital: " + country.capital;
+		capt.innerText = capital;
+		cardText.appendChild(capt);
 
-	let country = "afg";
+		let reg = document.createElement("div");
+		let region = "Region: " + country.region;
+		reg.innerText = region;
+		cardText.appendChild(reg);
 
-	fetch("https://restcountries.eu/rest/v2/name/" + country)
-		.then((Response) => {
-			return Response.json();
-		})
-		.then((data) => {
-			//console.log(data);
+		let CountryC = document.createElement("div");
+		let CountryCode = "Country Code: " + country.alpha3Code;
+		CountryC.innerText = CountryCode;
+		cardText.appendChild(CountryC);
 
-			//Adding Card Title to Card
-			cardHeader.innerText = data[0].name;
+		let weatherButton = document.createElement("button");
+		weatherButton.classList.add("btn", "btn-primary");
+		weatherButton.innerText = "Click for Weather";
 
-			let cardText = document.createElement("div");
-			cardText.classList.add("card-text");
-			cardBody.appendChild(cardText);
+		//Addding on click to get weather using fetch
+		weatherButton.addEventListener("click", async function () {
+			let response = await fetch(
+				"https://api.openweathermap.org/data/2.5/weather?q=" +
+					country.capital +
+					"&appid=" +
+					API_KEY
+			);
 
-			let capt = document.createElement("div");
-			let capital = "Capital: " + data[0].capital;
-			capt.innerText = capital;
-			cardText.appendChild(capt);
+			let weatherData = await response.json();
 
-			let reg = document.createElement("div");
-			let region = "Region: " + data[0].region;
-			reg.innerText = region;
-			cardText.appendChild(reg);
-
-			let CountryC = document.createElement("div");
-			let CountryCode = "Country Code: " + data[0].alpha3Code;
-			CountryC.innerText = CountryCode;
-			cardText.appendChild(CountryC);
-
-			let weatherButton = document.createElement("button");
-			weatherButton.classList.add("btn", "btn-primary");
-			weatherButton.innerText = "Click for Weather";
-			cardBody.appendChild(weatherButton);
-
-			weatherButton.addEventListener("click", function (event) {
-				//console.log("hi");
-				fetch(
-					"https://api.openweathermap.org/data/2.5/weather?q=" +
-						data[0].capital +
-						"&appid=" +
-						API_KEY
-				)
-					.then((Response) => {
-						return Response.json();
-					})
-					.then((value) => {
-						alert(
-							"Weather Data of " +
-								data[0].capital +
-								"\n" +
-								"Current Temperature: " +
-								value.main.temp +
-								"\n" +
-								"Condition:" +
-								value.weather[0].main +
-								"\n" +
-								"Humidity:" +
-								value.main.humidity +
-								"\n" +
-								"Wind Speed:" +
-								value.wind.speed
-						);
-					})
-					.catch((err) => {
-						console.error("Error in open weather api in afg block: " + err);
-					});
-			});
-		})
-		.catch((err) => {
-			console.error("Error in open weather api in afg block: " + err);
+			alert(
+				"Weather Data of " +
+					country.capital +
+					"\n" +
+					"Current Temperature: " +
+					(weatherData.main.temp - 273.15) +
+					" Degree Celcius" +
+					"\n" +
+					"Condition:" +
+					weatherData.weather[0].main +
+					"\n" +
+					"Humidity:" +
+					weatherData.main.humidity +
+					"\n" +
+					"Wind Speed:" +
+					weatherData.wind.speed
+			);
 		});
-})();
-//IIFE TO GENERATE CARD FOR NEPAL
-(() => {
-	let nepal = document.querySelector(".nepal");
 
-	let card = document.createElement("div");
-	card.classList.add("card", "text-center");
+		cardBody.appendChild(weatherButton);
+	} catch (e) {
+		console.error(e.message);
+	}
+}
 
-	let cardBody = document.createElement("div");
-	cardBody.classList.add("card-body");
+//Function to fetch rest Country Api and Add it to dom by caliing addcountry function
+(async function () {
+	try {
+		let response = await fetch("https://restcountries.eu/rest/v2/all");
 
-	let cardHeader = document.createElement("div");
-	cardHeader.classList.add("card-header");
+		let countries = await response.json();
 
-	card.appendChild(cardHeader);
-
-	let img = document.createElement("img");
-	img.classList.add("card-img-top");
-	img.setAttribute("src", "https://www.countryflags.io/np/shiny/64.png");
-	card.appendChild(img);
-
-	card.appendChild(cardBody);
-	nepal.appendChild(card);
-
-	let country = "np";
-
-	fetch("https://restcountries.eu/rest/v2/name/" + country)
-		.then((Response) => {
-			return Response.json();
-		})
-		.then((data) => {
-			//console.log(data);
-
-			//Adding Card Title to Card
-			cardHeader.innerText = data[0].name;
-
-			let cardText = document.createElement("div");
-			cardText.classList.add("card-text");
-			cardBody.appendChild(cardText);
-
-			let capt = document.createElement("div");
-			let capital = "Capital: " + data[0].capital;
-			capt.innerText = capital;
-			cardText.appendChild(capt);
-
-			let reg = document.createElement("div");
-			let region = "Region: " + data[0].region;
-			reg.innerText = region;
-			cardText.appendChild(reg);
-
-			let CountryC = document.createElement("div");
-			let CountryCode = "Country Code: " + data[0].alpha3Code;
-			CountryC.innerText = CountryCode;
-			cardText.appendChild(CountryC);
-
-			let weatherButton = document.createElement("button");
-			weatherButton.classList.add("btn", "btn-primary");
-			weatherButton.innerText = "Click for Weather";
-			cardBody.appendChild(weatherButton);
-
-			weatherButton.addEventListener("click", function (event) {
-				fetch(
-					"https://api.openweathermap.org/data/2.5/weather?q=" +
-						data[0].capital +
-						"&appid=" +
-						API_KEY
-				)
-					.then((Response) => {
-						return Response.json();
-					})
-					.then((value) => {
-						alert(
-							"Weather Data of " +
-								data[0].capital +
-								"\n" +
-								"Current Temperature: " +
-								value.main.temp +
-								"\n" +
-								"Condition:" +
-								value.weather[0].main +
-								"\n" +
-								"Humidity:" +
-								value.main.humidity +
-								"\n" +
-								"Wind Speed:" +
-								value.wind.speed
-						);
-					})
-					.catch((err) => {
-						console.error("Error in open weather api  in nepal block: " + err);
-					});
-			});
-		})
-		.catch((err) => {
-			console.error("Error in country api  in nepal block: " + err);
-		});
-})();
-
-//IIFE TO GENERATE CARD FOR Srilanka
-(() => {
-	let srilanka = document.querySelector(".srilanka");
-
-	let card = document.createElement("div");
-	card.classList.add("card", "text-center");
-
-	let cardBody = document.createElement("div");
-	cardBody.classList.add("card-body");
-
-	let cardHeader = document.createElement("div");
-	cardHeader.classList.add("card-header");
-
-	card.appendChild(cardHeader);
-
-	let img = document.createElement("img");
-	img.classList.add("card-img-top");
-	img.setAttribute("src", "https://www.countryflags.io/lk/shiny/64.png");
-	card.appendChild(img);
-
-	card.appendChild(cardBody);
-	srilanka.appendChild(card);
-
-	let country = "sri";
-
-	fetch("https://restcountries.eu/rest/v2/name/" + country)
-		.then((Response) => {
-			return Response.json();
-		})
-		.then((data) => {
-			//console.log(data);
-
-			//Adding Card Title to Card
-			cardHeader.innerText = data[0].name;
-
-			let cardText = document.createElement("div");
-			cardText.classList.add("card-text");
-			cardBody.appendChild(cardText);
-
-			let capt = document.createElement("div");
-			let capital = "Capital: " + data[0].capital;
-			capt.innerText = capital;
-			cardText.appendChild(capt);
-
-			let reg = document.createElement("div");
-			let region = "Region: " + data[0].region;
-			reg.innerText = region;
-			cardText.appendChild(reg);
-
-			let CountryC = document.createElement("div");
-			let CountryCode = "Country Code: " + data[0].alpha3Code;
-			CountryC.innerText = CountryCode;
-			cardText.appendChild(CountryC);
-
-			let weatherButton = document.createElement("button");
-			weatherButton.classList.add("btn", "btn-primary");
-			weatherButton.innerText = "Click for Weather";
-			cardBody.appendChild(weatherButton);
-
-			weatherButton.addEventListener("click", function (event) {
-				fetch(
-					"https://api.openweathermap.org/data/2.5/weather?q=" +
-						data[0].capital +
-						"&appid=" +
-						API_KEY
-				)
-					.then((Response) => {
-						return Response.json();
-					})
-					.then((value) => {
-						alert(
-							"Weather Data of " +
-								data[0].capital +
-								"\n" +
-								"Current Temperature: " +
-								value.main.temp +
-								"\n" +
-								"Condition:" +
-								value.weather[0].main +
-								"\n" +
-								"Humidity:" +
-								value.main.humidity +
-								"\n" +
-								"Wind Speed:" +
-								value.wind.speed
-						);
-					})
-					.catch((err) => {
-						console.error(
-							"Error in open weather api  in srilanka block: " + err
-						);
-					});
-			});
-		})
-		.catch((err) => {
-			console.error("Error in country api  in srilanka block: " + err);
-		});
+		for (let i = 0; i < countries.length; i++) {
+			AddCountry(countries[i]);
+		}
+	} catch (e) {
+		console.log(e.message);
+	}
 })();
